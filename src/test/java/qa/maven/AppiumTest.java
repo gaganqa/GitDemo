9384fe436da1a171a9ffb2f53a16bf62f55afc33
gagan <gagan_phg@hotmail.com>
2021-06-02 18:14:15 -0400

    GitX commit

--- src/test/java/qa/maven/AppiumTest.java
+++ src/test/java/qa/maven/AppiumTest.java
@@ -7,11 +7,14 @@ public class AppiumTest {
 	public void AndriodAutomation()
 	{
 		System.out.println("AndriodAutomation");
+		System.out.println("AndriodAutomation in depth");
+		
 	}
 	@Test
 	public void IOSautomation()
 	{
 		System.out.println("IOSautomation");
+		System.out.println("IOSautomation in depth ");
 	}
 
 }
